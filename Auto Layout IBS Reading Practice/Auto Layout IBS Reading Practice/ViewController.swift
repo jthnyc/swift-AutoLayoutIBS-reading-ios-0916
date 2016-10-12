c716722b4ee815d0a895f95fe0a61c0c3ee1a2b9
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-10-12 10:58:41 -0400

    Done

---
+++ Auto Layout IBS Reading Practice/Auto Layout IBS Reading Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout IBS Reading Practice
+//
+//  Created by Joanna Tzu-Hsuan Huang on 10/12/16.
+//  Copyright © 2016 Joanna Tzu-Hsuan Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Auto Layout IBS Reading Practice/Auto Layout IBS Reading PracticeTests/Auto_Layout_IBS_Reading_PracticeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Auto_Layout_IBS_Reading_PracticeTests.swift
+//  Auto Layout IBS Reading PracticeTests
+//
+//  Created by Joanna Tzu-Hsuan Huang on 10/12/16.
+//  Copyright © 2016 Joanna Tzu-Hsuan Huang. All rights reserved.
+//
+
+import XCTest
+@testable import Auto_Layout_IBS_Reading_Practice
+
+class Auto_Layout_IBS_Reading_PracticeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
